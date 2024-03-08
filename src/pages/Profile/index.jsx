5c99fc4460eacd3263319ad2d7c8bce58fb0b3c8
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-03-08 02:44:37 +0200

    smal fixies

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,12 @@
 const Profile = () => {
     return (
         <div className="profile">
-            {/* <div className="profile_info">
+            <div className="profile_info">
                 
             </div>
             <div className="">
-
-            </div> */}
+                
+            </div>
         </div>
         
     )
