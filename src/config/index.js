137fccc3e4a4ae7fea5bb80c1b5fcc596c8dae6f
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-04-05 01:37:57 +0300

    Change API_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3001' : 'https://news-site-frontend-rosy.vercel.app/posts'; 
+export const API_URL = process.env.API_URL;
