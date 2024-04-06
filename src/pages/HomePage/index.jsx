c56f5b07ad747a886e5e23de7605924ee57b12de
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-04-06 20:28:19 +0300

    Fixed banner

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import "./Posts.scss";
 import Banner from "../../components/Banner";
 import Posts from "../../components/Posts/index.jsx";
+import { API_URL } from "../../config/index.js";
 
 const HomePage = () => {
     
@@ -10,8 +11,8 @@ const HomePage = () => {
                 image={'https://img.tsn.ua/cached/552/tsn-dc382829a98d0f40b34d312a72bcb9b7/thumbs/1116x628/dc/7d/3769751a1240ebcc80e4c7322c177ddc.jpeg'}>
 
                 <h1>My News Site</h1>
-                <p>Please, share this work with your friends das das d sa das d sad sa dsa dasdasd asd as das d as d asd as dsa d as dsa friends das das d sa das d sad sa dsa dasdasd asd as das d as d asd as dsa d as dsa  </p>
-                <a href="/mccoklsa">MacKos</a>
+                <p>Please, share this work with your friends</p>
+                <a href={`${API_URL}/api/users/Maks`}>Maks</a>
             </Banner>
 
             <Posts/>
